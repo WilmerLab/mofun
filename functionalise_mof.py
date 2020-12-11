4aaf9b410ebb1f6f8f69842e6a1437942b164eb8
Paul Boone <narcissus.pong@gmail.com>
2020-12-11 10:46:59 -0500

    get first test working

--- functionalise_mof.py
+++ functionalise_mof.py
@@ -1,13 +1,19 @@
 import ase as ase
 from ase import Atoms, io
 
-atoms_structure = ase.io.read("structure")
-atoms_search = ase.io.read("search.pattern")
 
-# Search instances of first atom in a search pattern
-list = []
-for pattern_atom in atoms_search:
-    if pattern_atom.symbol == "C":
-        for structure_atom in atoms_structure:
-            if pattern_atom.symbol == structure_atom.symbol:
-                list.append(structure_atom.index)
+def find_pattern_in_structure(structure, pattern):
+    """find pattern in structure, where both are ASE atoms objects
+
+    Returns:
+        a list of indices of pattern found
+    """
+
+    # Search instances of first atom in a search pattern
+    list = []
+    for pattern_atom in pattern:
+        if pattern_atom.symbol == "C":
+            for structure_atom in structure:
+                if pattern_atom.symbol == structure_atom.symbol:
+                    list.append(structure_atom.index)
+    return list
