671c89801db0c789a94a8cfed2504c5b07b777ee
Paul Boone <narcissus.pong@gmail.com>
2020-12-15 14:35:24 -0500

    adjust tests to account for CH3 having 3 variations of CH2 within it geometrically; use a tolerate of 5e-2 (0.05 angstroms); refactor using atoms_of_type method to make a little simpler; reorganize for loop levels to be a little easier to follow

--- functionalise_mof.py
+++ functionalise_mof.py
@@ -3,6 +3,11 @@ import math
 import ase as ase
 from ase import Atoms, io
 
+
+
+def atoms_of_type(structure, element):
+    return [a for a in structure if element == a.symbol]
+
 def find_pattern_in_structure(structure, pattern):
     """find pattern in structure, where both are ASE atoms objects
 
@@ -10,39 +15,35 @@ def find_pattern_in_structure(structure, pattern):
         a list of indices of pattern found
     """
 
-
-    # now search for every other pattern atoms
     for i, pattern_atom_1 in enumerate(pattern):
         # Search instances of first atom in a search pattern
         if i == 0:
-            match_indices = []
-            for structure_atom in structure:
-                if pattern[0].symbol == structure_atom.symbol:
-                    match_indices.append([structure_atom.index])
-
+            match_indices = [[a.index] for a in atoms_of_type(structure, pattern[0].symbol)]
             print("round %d: " % i, match_indices)
             continue
 
         last_match_indices = match_indices
         match_indices = []
         for match in last_match_indices:
-            for structure_atom in structure:
-                if pattern_atom_1.symbol == structure_atom.symbol:
-                    found_match = True
-                    for j, pattern_atom_0 in enumerate(pattern[0:i]):
-                        pdist = pattern.get_distance(i, j)
-                        sdist = structure.get_distance(match[j], structure_atom.index)
-                        if not math.isclose(pdist, sdist, rel_tol = 1e-3):
-                            found_match = False
-                            break
-                    if found_match:
-                        print("found a match!", match, structure_atom.index)
-                        match_indices.append(match + [structure_atom.index])
+            for structure_atom in atoms_of_type(structure, pattern_atom_1.symbol):
+                found_match = True
+                for j, pattern_atom_0 in enumerate(pattern[0:i]):
+                    pdist = pattern.get_distance(i, j)
+                    sdist = structure.get_distance(match[j], structure_atom.index)
+                    print("[%d] %6.4f ~= %6.4f" % (pattern_atom_0.index, pdist, sdist))
+                    if not math.isclose(pdist, sdist, rel_tol=5e-2):
+                        found_match = False
+                        break
+
+                # anything that matches the distance to all prior pattern atoms is a good match so far
+                if found_match:
+                    print("found a match!", match, structure_atom.index)
+                    match_indices.append(match + [structure_atom.index])
         print("round %d: " % i, match_indices)
 
-    # anything still a match is a good match
-
     # remove duplicates
+    match_indices = set([tuple(sorted(indices)) for indices in match_indices])
+    print("after deduplication: ", match_indices)
 
     # get ASE atoms objects for each set of indices
     match_atoms = [structure.__getitem__(indices) for indices in match_indices]
--- tests/test_functionalise_mof.py
+++ tests/test_functionalise_mof.py
@@ -3,6 +3,7 @@ from importlib import resources
 
 import ase
 from ase import Atoms
+import pytest
 from pytest import approx
 
 from functionalise_mof import find_pattern_in_structure
@@ -20,18 +21,36 @@ def test_find_pattern_in_structure__find_all_carbons_in_octane():
         assert pattern_found.get_chemical_symbols() == ["C"]
 
 
+def test_find_pattern_in_structure__find_all_ch3_in_octane():
+    # CH3 CH2 CH2 CH2 CH2 CH2 CH2 CH3 #
+    with importlib.resources.path(tests, "octane.xyz") as octane_path:
+        structure = ase.io.read(octane_path)
+    print(structure)
+    pattern = Atoms('CHHH', positions=[(0, 0, 0), (-0.538, -0.635,  0.672), (-0.397,  0.993,  0.052), (-0.099, -0.371, -0.998)])
+    match_indices, match_atoms = find_pattern_in_structure(structure, pattern)
+    assert len(match_atoms) == 2
+    for pattern_found in match_atoms:
+        assert pattern_found.get_chemical_symbols() == ["C", "H", "H", "H"]
+        cpos = pattern_found[0].position
+        assert ((pattern_found[1].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
+        assert ((pattern_found[2].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
+        assert ((pattern_found[3].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
+
 def test_find_pattern_in_structure__find_all_ch2_in_octane():
     # CH3 CH2 CH2 CH2 CH2 CH2 CH2 CH3 #
     with importlib.resources.path(tests, "octane.xyz") as octane_path:
         structure = ase.io.read(octane_path)
+    print(structure)
     pattern = Atoms('CHH', positions=[(0, 0, 0),(-0.1  , -0.379, -1.017), (-0.547, -0.647,  0.685)])
     match_indices, match_atoms = find_pattern_in_structure(structure, pattern)
-    assert len(match_atoms) == 6
+
+    # there are technically 12 matches, since each CH3 makes 3 variations of CH2
+    assert len(match_atoms) == 12
     for pattern_found in match_atoms:
         assert pattern_found.get_chemical_symbols() == ["C", "H", "H"]
         cpos = pattern_found[0].position
-        assert ((pattern_found[1].position - cpos) ** 2).sum() == approx(1.18704299, 1e-3)
-        assert ((pattern_found[2].position - cpos) ** 2).sum() == approx(1.18704299, 1e-3)
+        assert ((pattern_found[1].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
+        assert ((pattern_found[2].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
 
 #
 # def find_pattern_in_structure():
