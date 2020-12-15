b646fced33239deaa7532e6a26907d3ed8bd7e8f
Paul Boone <narcissus.pong@gmail.com>
2020-12-14 21:34:08 -0500

    attempt at doing a full search; needs testing and removing from duplicates

--- functionalise_mof.py
+++ functionalise_mof.py
@@ -1,3 +1,5 @@
+import math
+
 import ase as ase
 from ase import Atoms, io
 
@@ -8,15 +10,35 @@ def find_pattern_in_structure(structure, pattern):
         a list of indices of pattern found
     """
 
-    # Search instances of first atom in a search pattern
-    match_indices = []
-    for i, pattern_atom in enumerate(pattern):
-        for structure_atom in structure:
-            if pattern_atom.symbol == structure_atom.symbol:
-                if i == 0: # first atom in pattern, no distance check
+
+    # now search for every other pattern atoms
+    for i, pattern_atom_1 in enumerate(pattern):
+        # Search instances of first atom in a search pattern
+        if i == 0:
+            match_indices = []
+            for structure_atom in structure:
+                if pattern[0].symbol == structure_atom.symbol:
                     match_indices.append([structure_atom.index])
-                else:
-                    raise Exception("needs to calculate distances here")
+
+            print("round %d: " % i, match_indices)
+            continue
+
+        last_match_indices = match_indices
+        match_indices = []
+        for match in last_match_indices:
+            for structure_atom in structure:
+                if pattern_atom_1.symbol == structure_atom.symbol:
+                    found_match = True
+                    for j, pattern_atom_0 in enumerate(pattern[0:i]):
+                        pdist = pattern.get_distance(i, j)
+                        sdist = structure.get_distance(match[j], structure_atom.index)
+                        if not math.isclose(pdist, sdist, rel_tol = 1e-3):
+                            found_match = False
+                            break
+                    if found_match:
+                        print("found a match!", match, structure_atom.index)
+                        match_indices.append(match + [structure_atom.index])
+        print("round %d: " % i, match_indices)
 
     # anything still a match is a good match
 
