b5e4f3292d85497f7a55f25e356fae244fd129da
Paul Boone <narcissus.pong@gmail.com>
2020-12-16 11:51:15 -0500

    remove distance test for PBC test since doesn't work across PBCs

--- tests/test_functionalise_mof.py
+++ tests/test_functionalise_mof.py
@@ -21,7 +21,6 @@ def test_find_pattern_in_structure__octane_has_8_carbons():
     for pattern_found in match_atoms:
         assert pattern_found.get_chemical_symbols() == ["C"]
 
-
 def test_find_pattern_in_structure__octane_has_2_CH3():
     # CH3 CH2 CH2 CH2 CH2 CH2 CH2 CH3 #
     with importlib.resources.path(tests, "octane.xyz") as octane_path:
@@ -65,13 +64,8 @@ def test_find_pattern_in_structure__octane_over_pbc_has_2_CH3():
         structure.set_positions(positions)
         structure.set_cell(15 * np.identity(3))
 
-    print(structure.cell)
     pattern = Atoms('CHHH', positions=[(0, 0, 0), (-0.538, -0.635,  0.672), (-0.397,  0.993,  0.052), (-0.099, -0.371, -0.998)])
     match_indices, match_atoms = find_pattern_in_structure(structure, pattern)
     assert len(match_atoms) == 2
     for pattern_found in match_atoms:
         assert pattern_found.get_chemical_symbols() == ["C", "H", "H", "H"]
-        cpos = pattern_found[0].position
-        assert ((pattern_found[1].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
-        assert ((pattern_found[2].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
-        assert ((pattern_found[3].position - cpos) ** 2).sum() == approx(1.18704299, 5e-2)
