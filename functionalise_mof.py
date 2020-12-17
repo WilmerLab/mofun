ec9f9e04b408a532dc9e344bcd4c63ac93f85e0c
Paul Boone <narcissus.pong@gmail.com>
2020-12-16 20:43:04 -0500

    use indices and positions and types directly without ASE objects to optimize for speed (also may be a little simpler to understand)

--- functionalise_mof.py
+++ functionalise_mof.py
@@ -5,14 +5,15 @@ from ase import Atoms, io
 import numpy as np
 from numpy.linalg import norm
 
-def atoms_of_type(structure, element):
-    return [a for a in structure if element == a.symbol]
+def atoms_of_type(types, element):
+    """ returns all atom indices in types that match the symbol element """
+    return [i for i, t in enumerate(types) if t == element]
 
-def uc_neighbor_offsets(structure):
+def uc_neighbor_offsets(uc_vectors):
     multipliers = np.array(np.meshgrid([-1, 0, 1],[-1, 0, 1],[-1, 0, 1])).T.reshape(-1, 3)
     # offsets = np.array([[0, 0, 0]])
     # return np.array([(structure.cell * m).sum(axis=1) for m in multipliers]).flatten().reshape(27, 3)
-    return [tuple((structure.cell * m).sum(axis=1)) for m in multipliers]
+    return [tuple((uc_vectors * m).sum(axis=1)) for m in multipliers]
 
 
 def find_pattern_in_structure(structure, pattern):
@@ -22,13 +23,19 @@ def find_pattern_in_structure(structure, pattern):
         a list of indice lists for each set of matched atoms found
     """
 
-    uc_offsets = uc_neighbor_offsets(structure)
+    uc_offsets = uc_neighbor_offsets(structure.cell)
     print(uc_offsets)
+
+    s_positions = structure.positions
+    s_types = list(structure.symbols)
+
+    p_positions = pattern.positions
+    p_types = list(pattern.symbols)
 
     for i, pattern_atom_1 in enumerate(pattern):
         # Search instances of first atom in a search pattern
         if i == 0:
-            match_indices = [[(a.index,  (0., 0., 0.))] for a in atoms_of_type(structure, pattern[0].symbol)]
+            match_indices = [[(idx,  (0., 0., 0.))] for idx in atoms_of_type(s_types, p_types[0])]
             print("round %d: " % i, match_indices)
             continue
 
@@ -38,16 +45,16 @@ def find_pattern_in_structure(structure, pattern):
         for match in last_match_indices:
             print("----------------------------------")
             print(match)
-            for structure_atom in atoms_of_type(structure, pattern_atom_1.symbol):
+            for atom_idx in atoms_of_type(s_types, pattern_atom_1.symbol):
                 print("--")
                 for uc_offset in uc_offsets:
                     found_match = True
-                    for j, pattern_atom_0 in enumerate(pattern[0:i]):
-                        pdist = pattern.get_distance(i, j)
-                        match_atom = structure[match[j][0]]
+                    for j in range(i):
+                        pdist = norm(p_positions[j] - p_positions[i])
+                        match_idx = match[j][0]
                         match_offset = match[j][1]
                         # print("NORM ARGS: ", structure[match[j]].position + uc_offset, structure_atom.position)
-                        sdist = norm(match_atom.position + match_offset - structure_atom.position - uc_offset)
+                        sdist = norm(s_positions[match_idx] + match_offset - s_positions[atom_idx] - uc_offset)
 
                         if not math.isclose(pdist, sdist, rel_tol=5e-2):
                             # print("[%d] %6.4f != %6.4f" % (pattern_atom_0.index, pdist, sdist))
@@ -57,8 +64,8 @@ def find_pattern_in_structure(structure, pattern):
                         # print("[%d] %6.4f ~= %6.4f" % (pattern_atom_0.index, pdist, sdist))
                     # anything that matches the distance to all prior pattern atoms is a good match so far
                     if found_match:
-                        print("found a match!", match, structure_atom.index, uc_offset)
-                        match_indices.append(match + [(structure_atom.index, uc_offset)])
+                        print("found a match!", match, atom_idx, uc_offset)
+                        match_indices.append(match + [(atom_idx, uc_offset)])
 
                         #TODO: need to save image index for MATCH!!
 
