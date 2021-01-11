4eed8d00a98a7fba41dca13527ad65d6f91dd226
meiirbek.islamov <mei12@pitt.edu>
2021-01-11 12:20:24 -0500

    add a function to translate the pattern so that the first defined atom's position is at 0,0,0

--- functionalise_mof.py
+++ functionalise_mof.py
@@ -128,3 +128,21 @@ def rotate_replace_pattern(pattern, pivot_atom_index, axis, angle):
         pattern[i].position[2] = z0 + dzr
 
     return pattern
+
+def translate_molecule_origin(pattern):
+
+    # Translate the molecule so that the first defined atom's position is at 0,0,0
+
+    first_atom_position = pattern[0].position
+    origin = np.array([0,0,0])
+
+    dx = origin[0] - first_atom_position[0]
+    dy = origin[1] - first_atom_position[1]
+    dz = origin[2] - first_atom_position[2]
+
+    for position in pattern.positions:
+        position[0] += dx
+        position[1] += dy
+        position[2] += dz
+
+    return pattern
