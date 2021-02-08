1718db4fb0b3a97d3dd7d6de853337919f33544b
Paul Boone <narcissus.pong@gmail.com>
2021-02-08 16:21:51 -0500

    add bond_types to atoms initialization so that we don't error out when lengthes of bonds and bond types are different

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -59,7 +59,7 @@ def assert_benzene(coords):
 @pytest.fixture
 def linear_cnnc():
     yield Atoms('CNNC', positions=[(0., 0., 0), (1.0, 0., 0.), (2.0, 0., 0.), (3.0, 0., 0.)],
-                bonds=[(0,1), (1,2), (2,3)], cell=15*np.identity(3))
+                bonds=[(0,1), (1,2), (2,3)], bond_types=[1] * 3, cell=15*np.identity(3))
 
 @pytest.fixture
 def octane():
