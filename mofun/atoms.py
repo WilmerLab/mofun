3388808281bc70f442301fe9e14a705231e7b62c
Paul Boone <narcissus.pong@gmail.com>
2021-09-22 12:18:17 -0400

    add to rough_uff routines for assigning UFF force field parameters to Atoms objects

--- mofun/atoms.py
+++ mofun/atoms.py
@@ -1,6 +1,5 @@
 import copy
 import io
-import itertools
 import os
 import pathlib
 import xml.etree.ElementTree as ET
@@ -8,7 +7,7 @@ import xml.etree.ElementTree as ET
 import ase
 from ase.formula import Formula
 from CifFile import ReadCif as read_cif
-import networkx as nx
+
 import numpy as np
 from scipy.linalg import norm
 
@@ -932,30 +931,6 @@ class Atoms:
         if self.cell is not None and len(self.cell) > 0:
             kwargs['cell'] = self.cell
         return ase.Atoms(self.elements, **kwargs)
-
-    def calc_angles(self):
-        g = nx.Graph()
-        g.add_edges_from([tuple(x) for x in self.bonds])
-
-        angles = []
-        for n in g.nodes:
-            angles += [(a, n, b) for (a,b) in itertools.combinations(g.neighbors(n), 2)]
-        self.angles = np.array(angles)
-
-    def calc_dihedrals(self):
-        g = nx.Graph()
-        g.add_edges_from([tuple(x) for x in self.bonds])
-
-        dihedrals = []
-        for a, b in g.edges:
-            a_neighbors = list(g.adj[a])
-            a_neighbors.remove(b)
-            b_neighbors = list(g.adj[b])
-            b_neighbors.remove(a)
-
-            dihedrals += [(a1, a, b, b1) for a1 in a_neighbors for b1 in b_neighbors]
-        self.dihedrals = np.array(dihedrals)
-
 
 def find_unchanged_atom_pairs(orig_structure, final_structure, max_delta=1e-5):
     """Returns array of tuple pairs, where each pair contains the indices in the original and the final
